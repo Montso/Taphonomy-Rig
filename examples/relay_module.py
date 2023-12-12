f59b5724d3d078c27ef768cdf2c4e43787765b18
Montso <montsomokake04@gmail.com>
2023-12-12 20:41:59 +0000

    Deleted gsm code. Rewrote relay_module.py

--- examples/relay_module.py
+++ examples/relay_module.py
@@ -1,53 +1,69 @@
 import RPi.GPIO as GPIO
-import time
 
-class relay_module:
+class relay_module():
 
-    def __init__(self, pin1, pin2, low_state = True):
+    HIGH = 1
+    LOW = 0
 
-        self.low_state = low_state
+    def __init__(self, pin1:int, pin2:int, default_state = LOW):
+        """
+        Parameters:
+        -INx: input pin for relay x 
+        -default_state: default relay state
+        """
+        self.defaul_state = default_state
         self.pin1 = pin1
         self.pin2 = pin2
+
+        # GPIO initilaisation
 
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(pin1,GPIO.OUT)
         GPIO.setup(pin2,GPIO.OUT)
-        GPIO.output(pin1,low_state)
-        GPIO.output(pin2,low_state)
+        GPIO.output(pin1,default_state)
+        GPIO.output(pin2,default_state)
 
-        self.led1_state = low_state
-        self.led2_state = low_state
+        # initialise relay to NO
+        self.led1_state = default_state
+        self.led2_state = default_state
 
-    def set_high(self, which_pin):
-        if(which_pin == 1):
-            self.led1_state = not self.low_state
-            GPIO.output(self.pin1,self.led1_state)
-        elif(which_pin == 2):
-            self.led2_state = not self.low_state
-            GPIO.output(self.pin2,self.led2_state)
+    def set_high(self, pin:int):
+        match pin:
+            case 1:
+                GPIO.output(self.pin1,relay_module.HIGH)
+                self.led1_state = relay_module.HIGH
+            case 2:
+                GPIO.output(self.pin2,relay_module.HIGH)
+                self.led2_state = relay_module.HIGH
         
 
-    def set_low(self, which_pin):
-        if(which_pin == 1):
-            self.led1_state = self.low_state
-            GPIO.output(self.pin1,self.led1_state)
-        elif(which_pin == 2):
-            self.led2_state = self.low_state
-            GPIO.output(self.pin2,self.led2_state)
+    def set_low(self, pin):
+        match pin:
+            case 1:
+                GPIO.output(self.pin1,relay_module.LOW)
+                self.led1_state = relay_module.LOW
+            case 2:
+                GPIO.output(self.pin2,relay_module.LOW)
+                self.led2_state = relay_module.LOW
 
-    def set_state(self, which_pin, which_state):
-        if(which_state == 0):
-            self.set_low(which_pin)
-        elif(which_state == 1):
-            self.set_high(which_pin)
+    def set_state(self, pin, state):
+        match state:
+            case relay_module.HIGH:
+                self.set_high(pin)
+            case relay_module.LOW:
+                self.set_low(pin)
 
-    def get_state(self, which_pin):
-        if which_pin == 1:
-            return self.led1_state
-        elif which_pin == 2:
-            return self.led2_state
-        else:
-            return -1
+    def get_state(self, pin):
+        match pin:
+            case 1: 
+                return self.led1_state
+            case 2:
+                return self.led2_state
+            case _:
+                return -1
 
-    def toggle_state(self, which_pin):
-        self.set_state(which_pin,self.get_state())
+    def toggle_state(self, pin):
+        if pin != 1 or pin != 2:
+            print("Pin number should be 1 or 2.")
+            quit
+        self.set_state(pin, not self.get_state())
--- scripts/stop_gsm.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash 
-echo "Service Started"
-while [ ! -c /dev/cdc-wdm0 ];
-do
-	sleep 1
-	echo "waiting for device"
-done;
-sudo qmicli -d /dev/cdc-wdm0 --dms-set-operating-mode='low-power'
-sudo ip link set wwan0 down
