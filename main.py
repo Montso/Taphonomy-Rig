1ff94fabe13d78387a5c1b323e373a5546593fed
Montso <montsomokake04@gmail.com>
2024-01-07 23:17:26 +0000

    Startup code done.

--- main.py
+++ main.py
@@ -1,62 +1,110 @@
+from email.policy import default
+from hx711 import HX711
+import logging
+import numpy as np
+import os
+from examples.config import LOWER_PIN
+from relay import Lift
 import RPi.GPIO as GPIO  # import GPIO
 from scale import Scale  # import the class HX711
+import statistics
+import time
+import yaml
 
-try:
-    GPIO.setmode(GPIO.BCM)  # set GPIO pin mode to BCM numbering
-    # Create an object hx which represents your real hx711 chip
-    # Required input parameters are only 'dout_pin' and 'pd_sck_pin'
-    hx = Scale(dout_pin=21, pd_sck_pin=20)
-    # measure tare and save the value as offset for current channel
-    # and gain selected. That means channel A and gain 128
-    err = hx.zero()
-    # check if successful
-    if err:
-        raise ValueError('Tare is unsuccessful.')
+# SETUP
 
-    reading = hx.get_raw_data_mean()
-    if reading:  # always check if you get correct value or only False
-        # now the value is close to 0
-        print('Data subtracted by offset but still not converted to units:',
-              reading)
-    else:
-        print('invalid data', reading)
+#Load config data
+conf_file = open("default_config.yaml", 'r')
+conf = yaml.safe_load(conf_file)
+conf_file.close()
 
-    # In order to calculate the conversion ratio to some units, in my case I want grams,
-    # you must have known weight.
-    input('Put known weight on the scale and then press Enter')
-    reading = hx.get_data_mean()
-    if reading:
-        print('Mean value from HX711 subtracted by offset:', reading)
-        known_weight_grams = input(
-            'Write how many grams it was and press Enter: ')
-        try:
-            value = float(known_weight_grams)
-            print(value, 'grams')
-        except ValueError:
-            print('Expected integer or float and I have got:',
-                  known_weight_grams)
+#Config variables
+VERSION = conf["Device"]["Version"]
+DEVICE_ID = conf["Device"]["ID"]
 
-        # set scale ratio for particular channel and gain which is
-        # used to calculate the conversion to units. Required argument is only
-        # scale ratio. Without arguments 'channel' and 'gain_A' it sets
-        # the ratio for current channel and gain.
-        ratio = reading / value  # calculate the ratio for channel A and gain 128
-        hx.set_scale_ratio(ratio)  # set ratio for current channel
-        print('Ratio is set.')
-    else:
-        raise ValueError('Cannot calculate mean value. Try debug mode. Variable reading:', reading)
+#Pig
+AVERAGE_WEIGHT = conf["Pig"]["Avg_Weight"]
+LIFT_PIG_FLAG = conf["Pig"]["Lift_pig"]
+#Scale
+SCALE_OFFSET = conf["Scale"]["OFFSET"]
+CAL_FACTOR = conf["Scale"]["RATIO"]
+SCALE_READOUTS = conf["Scale"]["Default_readouts"]
 
-    # Read data several times and return mean value
-    # subtracted by offset and converted by scale ratio to
-    # desired units. In my case in grams.
-    print("Now, I will read data in infinite loop. To exit press 'CTRL + C'")
-    input('Press Enter to begin reading')
-    print('Current weight on the scale in grams is: ')
-    while True:
-        print(hx.get_weight_mean(20), 'g')
+#Relay
+LOWER_PIN = conf["Relay"]["LOWER_PIN"]
+LIFT_PIN = conf["Relay"]["LIFT_PIN"]
+LIFT_DELAY = conf["Relay"]["DELAY"]
 
-except (KeyboardInterrupt, SystemExit):
-    print('Bye :)')
+#Config variables//
 
-finally:
-    GPIO.cleanup()
+
+#Logging stream
+logging.basicConfig(filename = "./log/file_{t}.log".format(t = time.strftime("%Y_%m_%d_%H_%M_%S", time.localtime())),level=logging.DEBUG, format="%(asctime)s:" + logging.BASIC_FORMAT)
+console = logging.StreamHandler()
+console.setLevel(logging.INFO)
+console.setFormatter(logging.Formatter("%(asctime)s:" + logging.BASIC_FORMAT))
+logging.getLogger().addHandler(console)
+logger = logging.getLogger(__name__)
+
+timestamp = time.time()
+message_dump = ""
+
+def log():
+    global output, message_dump
+    message_dump += f"{output}\n"
+    logger.info(output)
+
+#Lift Startup log
+output = "Regular Nightly Lift Program"
+log()
+output = "Using hardcoded calibration factor of %i and offset %i" %(CAL_FACTOR, SCALE_OFFSET)
+log()
+output = "The lifting flag is set to %r" %LIFT_PIG_FLAG
+log()
+time.sleep(2)
+
+output = "Piglift startup"
+log()
+
+#Setup
+GPIO.setmode(GPIO.BCM)  # set GPIO pin mode to BCM numbering
+
+platform = Lift(
+    pin1=22,
+    pin2=5,
+    default_state=False,
+    lift_pin=LIFT_PIN,
+    lower_pin=LOWER_PIN) # Handles the wrench lifting/lowering
+
+hx = HX711(pd_sck_pin=19, dout_pin=16)
+hx.power_up()
+output = "Resetting scale..."
+log()
+hx.reset()
+
+def get_weight(readings=SCALE_READOUTS):
+    """
+    Calculates weight by (reading - offset)/calc_factor
+    """
+    global hx
+    raw_read = hx.get_raw_data_mean(SCALE_READOUTS)
+    weight = round((((raw_read-SCALE_OFFSET)/CAL_FACTOR)**2)**0.5, 3)
+    
+    return weight
+
+#SETUP//
+
+output = "Printing %i test readings"%SCALE_READOUTS
+log()
+
+for _ in range(5): #random number to be updated
+    val = get_weight()
+    output = str(val)
+    log()
+    time.sleep(0.05)
+
+#LIFT Procedure
+output = "performing a lift in %i seconds" % conf.peaceful_pause
+
+
+GPIO.cleanup()
