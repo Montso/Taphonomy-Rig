787494a82a5fc0c3c7d195a4e3e0888e9857cd85
Justin Pead <justinpead@gmail.com>
2024-07-03 01:09:37 +0200

    testing hooks3

--- update-version.sh
+++ update-version.sh
@@ -5,7 +5,7 @@ VERSION_NUMBER=$(git rev-list --count HEAD)
 COMMIT_HASH=$(git rev-parse HEAD)
 
 # Path to the YAML file
-YAML_FILE="~/Taphony-rig/config.yaml"
+YAML_FILE="~/Taphonomy-rig/config.yaml"
 
 # Update the YAML file with the commit hash
 python3 ~/Taphonomy-rig/update_version.py $YAML_FILE $COMMIT_HASH $VERSION_NUMBER
